2e542f05cdd4f5a80286a6a681235ec14730f2b5
Kunsh-Manghwani <66488262+Kunsh-Manghwani@users.noreply.github.com>
2020-08-14 10:10:07 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+
+struct node{
+	int i;
+	struct node *link;
+}*first,*n,*ptr,*tptr;
+
+void add(){
+	n = (struct node*)malloc(sizeof(struct node));
+	printf("\nValue : ");
+	scanf("%d",&n->i);
+	n->link = NULL;
+	ptr = first;
+	while(ptr->link!=NULL){
+		ptr = ptr->link;
+	}
+	ptr->link = n;
+}
+
+void display(){
+	ptr = first;
+	if(first == NULL){
+		printf("\nERROR LINK IS EMPTY !\n");
+	}
+	else{
+		while(ptr!=NULL){
+			printf("%d ",ptr->i);
+			ptr = ptr->link;
+		}
+	}
+}
+
+int main(){
+	ptr = (struct node*)malloc(sizeof(struct node));
+	printf("\nValue : ");
+	scanf("%d",&ptr->i);
+	ptr->link=NULL;
+	first = ptr;
+	
+	char ch ;
+	fflush(stdin);
+	while(1){
+		fflush(stdin);
+		scanf("%c",&ch);
+		if(ch != 'y'){
+			break;
+		}
+		add();
+	}
+	
+	display();
+}
